5c0649e76e7ae572535e1818c5d1cfde0c8e8d77
blackspy03 <106814890+blackspy03@users.noreply.github.com>
2025-10-22 15:11:15 +0700

    Update main.js

--- Calculator/main.js
+++ Calculator/main.js
@@ -14,6 +14,12 @@ let A = 0;
 let operator = null;
 let B = null;
 
+function clearAll() {
+    A = 0;
+    B = null;
+    operator = null;
+}
+
 for (let i = 0; i < buttonValues.length; i++) {
     let value = buttonValues[i];
     let button = document.createElement("button");
@@ -33,9 +39,42 @@ for (let i = 0; i < buttonValues.length; i++) {
     //add event to button
     button.addEventListener("click", function () {
         if (rightSymbols.includes(value)) {
-
+            if (value == "=") {
+                if (A != null) {
+                    let numA = Number(A);
+                    let numB = Number(display.value);
+                    if (operator == "+") {
+                        display.value = numA + numB;
+                    } else if (operator == "-") {
+                        display.value = numA - numB;
+                    } else if (operator == "×") {
+                        display.value = numA * numB;
+                    } else if (operator == "÷") {
+                        display.value = numA / numB;
+                    }
+                    A = Number(display.value);
+                    clearAll();
+                }
+            } else {
+                operator = value;
+                A = display.value;
+                display.value = "";
+            }
         } else if (topSymbols.includes(value)) {
-
+            if (value == "AC") {
+                display.value = "0";
+                clearAll();
+            } else if (value == "+/-") {
+                if (display.value != "0" && display.value != "") {
+                    if (display.value[0] == "-") {//remove minus
+                        display.value = display.value.slice(1);
+                    } else {
+                        display.value = "-" + display.value;
+                    }
+                }
+            } else if (value == "%") {
+                display.value = Number(display.value) / 100;
+            }
         } else { //number
             if (value == ".") {
                 if (display.value != "" && !display.value.includes(value)) {
